5e7c2cf2368457484541d552ebc11c97764202e2
zc <459444677@qq.com>
2020-09-26 16:29:37 +0800

    main i

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+        int i = 1;
+        //test windows fix
+        int ii = 2;
     }
 }
