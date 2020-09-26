bc0b3aa6a601e17d03a8cf5cbc96593c2064dfdb
张琛 <zhangchen03@baijiahulian.com>
2020-09-26 17:52:28 +0800

    mac1752

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String str = "form tp";
+        String str1 ="test form mac";
     }
 }
